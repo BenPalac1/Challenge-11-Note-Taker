26e09d9c1dc7c85b0331fde6e178a6ca8ac14395
BenPalac1 <ben.palacpac@gmail.com>
2024-01-09 16:52:50 -0500

    fixed GET and POST api routes and tested, WOrks YEEEES!

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,36 +1,37 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const fs = require ("fs");
+const fs = require("fs").promises;
 
-//GET /api/notes should read the db.json file and return all saved notes as JSON
+const dbPath = "db/db.json";
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON
 router.get('/api/notes', async (req, res) => {
-  const userNotes = await JSON.parse(fs.readFileSync("db/db.json","utf8"));
-  res.json(userNotes);
+  try {
+    const userNotes = await fs.readFile(dbPath, 'utf8');
+    res.json(JSON.parse(userNotes));
+  } catch (err) {
+    console.err('Error reading notes:', err);
+    res.status(500).send('Server Error!');
+  }
 });
 
-//POST /api/notes should receive a new note to save on the request body. add it to the db.json file, and then return the new note to the client.
-router.post('/api/notes', (req, res) => {
-  const userNotes = JSON.parse(fs.readFileSync("db/db.json","utf8"));
-  const newNote = {
-    title: req.body.title,
-    text: req.body.text,
-    id: uuidv4(),
-  };
+// POST /api/notes should receive a new note to save on the request body. Add it to the db.json file and then return the new note to the client.
+router.post('/api/notes', async (req, res) => {
+  try {
+    const userNotes = JSON.parse(await fs.readFile(dbPath, 'utf8'));
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uuidv4(),
+    };
 
-  userNotes.push(newNote);
-  fs.writeFileSync("db/db.json",JSON.stringify(userNotes));
-  res.json(userNotes);
+    userNotes.push(newNote);
+    await fs.writeFile(dbPath, JSON.stringify(userNotes));
+    res.json(newNote);
+  } catch (err) {
+    console.err('Error Note note Saved!:', err);
+    res.status(500).send('Server Error!');
+  }
 });
 
-// router.delete('/api/notes/:id', (req, res) => {
-//   let data = fs.readFileSync("db/db.json", "utf8");
-//   const dataJSON =  JSON.parse(data);
-//   const newNotes = dataJSON.filter((note) => { 
-//     return note.id !== req.params.id;
-//   });
-  
-//   fs.writeFileSync("db/db.json",JSON.stringify(newNotes));
-//   res.json("Note deleted.");
-// });
-
-module.exports = router; 
+module.exports = router;
